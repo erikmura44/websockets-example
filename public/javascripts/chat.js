4ad49bb041960cde20e998f3429a8bc99992d2e4
erikmura44 <erikmura44@gmail.com>
2016-09-19 12:41:27 -0600

    websockets

---
+++ public/javascripts/chat.js
@@ -0,0 +1,13 @@
+$(function(){
+  var socket = io();
+  $('.chatBox').submit(function(event){
+    event.preventDefault();
+    var message = $("#message").val();
+    socket.emit('chat message', message);
+    $("#message").val("");
+  });
+
+  socket.on('chat message', function(data){
+    $("#messages").append(`<li>${data}</li>`);
+  });
+});
