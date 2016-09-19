2021981efcaf7f5ed9f2ba0113a97aac12e080e2
erikmura44 <erikmura44@gmail.com>
2016-09-19 15:53:35 -0600

    updated client side

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,14 +1,27 @@
+//CLIENT-SIDE
+
 $(function(){
+
   var socket = io();
-  $(".chatBox").submit(function(event){
+
+  $("form").submit(function(event){
+    //Prevent form from submitting and store user input in userMessage variable
     event.preventDefault();
-    var message = $("#message").val();
-    socket.emit('chat message', message);
-    $("#message").val("");
-    return false;
+    let userMessage = $('#m').val();
+
+    //Emits an event to sockets.js identified by the string 'send message' and passes the object {message: userMessage}
+    socket.emit('send message', {
+      message: userMessage
+    });
+
+    //Clears input value
+    $('#m').val('');
+
   });
 
-  socket.on('chat message', function(data){
-    $("#messages").append(`<li>${data}</li>`);
+  //Listens for 'send message' event, appends m.message to the messages ul when a 'send message' event is received from sockets.js
+  socket.on('send message', (m) => {
+    $('#messages').append(`<li>${m.message}</li>`);
   });
+
 });
